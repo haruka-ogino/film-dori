70f508aa1e678aa0d908c46c63be890c03a3c584
Haruka Ogino <harukariq@gmail.com>
2024-08-06 13:45:19 +1200

    import font

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,15 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import NavBar from '@/components/NavBar'
+import { Single_Day } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
+const singleDay_init = Single_Day({
+  // subsets:['latin'],
+  weight: ['400'],
+  variable: '--font-single_day',
+})
 export const metadata: Metadata = {
   title: 'All the Ghibli Things!',
   description: 'A library for all Ghibli things',
@@ -17,7 +23,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body
+        className={`${inter.className} ${singleDay_init.variable} single_day`}
+      >
+        <h1 className="single_day">All the Ghibli Things!</h1>
         <NavBar />
         {children}
       </body>
