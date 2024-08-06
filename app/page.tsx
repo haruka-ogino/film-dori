8f296f414c32e03c18775d645e436569a1ce036b
Haruka Ogino <harukariq@gmail.com>
2024-08-06 14:06:45 +1200

    make home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,16 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1>Welcome to All the Ghibli Thing!</h1>
+      <p>
+        A place where you can see films, characters, dishes and more from the
+        Ghibli Studio films.
+      </p>
+      <p>
+        Test your Ghibli knowledge with the quiz, or tease your brain with a
+        jigsaw puzzle.
+      </p>
+    </main>
   )
 }
