0492f45604efb76b04fc2e36499c3a4bd8961cee
Haruka Ogino <harukariq@gmail.com>
2024-08-06 14:03:00 +1200

    style navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,8 +1,12 @@
 const NavBar = () => {
   return (
-    <>
-      <h1>Nav</h1>
-    </>
+    <nav className="flex justify-between items-center overflow-x-hidden mt-2">
+      <h2>item 1</h2>
+      <h2>item 2</h2>
+      <h2>item 3</h2>
+      <h2>item 4</h2>
+      <h2>item 5</h2>
+    </nav>
   )
 }
 
