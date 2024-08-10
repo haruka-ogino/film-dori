923caaae96cf893cdf1971fb6539c8348b5d47cc
Haruka Ogino <harukariq@gmail.com>
2024-08-10 14:13:26 +1200

    update description of site

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <h1>Welcome to All the Ghibli Thing!</h1>
-      <p>
+      <h1>Welcome!</h1>
+      {/* <p>
         A place where you can see films, characters, dishes and more from the
         Ghibli Studio films.
       </p>
       <p>
         Test your Ghibli knowledge with the quiz, or tease your brain with a
         jigsaw puzzle.
+      </p> */}
+      <p>
+        The place where you can share locations that inspired your favourite
+        films and tv shows!
       </p>
     </main>
   )
