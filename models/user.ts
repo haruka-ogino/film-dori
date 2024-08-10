48018d70c09ed8fe864bb85ae6513a0e4f6d1a57
Haruka Ogino <harukariq@gmail.com>
2024-08-10 14:31:23 +1200

    create user model

---
+++ models/user.ts
@@ -0,0 +1,33 @@
+import { turso } from '@/utils/database'
+
+export const createUser = async (userData: {
+  email: string
+  username: string
+  image: string
+}) => {
+  const query = `
+    INSERT INTO users (email, username, image)
+    VALUES (?, ?, ?)
+  `
+
+  const args = [userData.email, userData.username, userData.image]
+
+  await turso.execute({ sql: query, args })
+}
+
+export const findUserByEmail = async (email: string) => {
+  const query = `
+    SELECT * FROM users WHERE email = ?
+  `
+  const result = await turso.execute({ sql: query, args: [email] })
+  if (result.rows.length > 0) {
+    const [user] = result.rows
+    return {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+      image: user.image,
+    }
+  }
+  return result.rows.length > 0 ? result.rows[0] : null
+}
