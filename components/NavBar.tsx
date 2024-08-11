0c818f9001ab89fdb5aaea103f629da03fa41f09
Haruka Ogino <harukariq@gmail.com>
2024-08-11 21:04:57 +1200

    implement signIn on FE

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,4 +1,36 @@
+'use client'
+
+import Link from 'next/link'
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import {
+  signIn,
+  signOut,
+  useSession,
+  getProviders,
+  LiteralUnion,
+  ClientSafeProvider,
+} from 'next-auth/react'
+import { BuiltInProviderType } from 'next-auth/providers/index'
+
 const NavBar = () => {
+  const { data: session } = useSession()
+
+  const [providers, setProviders] = useState<Record<
+    LiteralUnion<BuiltInProviderType, string>,
+    ClientSafeProvider
+  > | null>(null)
+  const [toggleDropdown, setToggleDropdown] = useState(false)
+
+  useEffect(() => {
+    const setUpProviders = async () => {
+      const res = await getProviders()
+      setProviders(res)
+    }
+
+    setUpProviders()
+  }, [])
+
   return (
     <nav className="flex justify-between items-center overflow-x-hidden mt-2">
       <h2>item 1</h2>
@@ -6,6 +38,50 @@ const NavBar = () => {
       <h2>item 3</h2>
       <h2>item 4</h2>
       <h2>item 5</h2>
+      {/* Desktop Navigation */}
+      <div className="sm:flex hidden">
+        {session?.user ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+
+            <button
+              type="button"
+              onClick={() => signOut()}
+              className="outline_btn"
+            >
+              Sign Out
+            </button>
+
+            <Link href="/profile">
+              <Image
+                src={session?.user.image ?? ''}
+                width={37}
+                height={37}
+                className="rounded-full"
+                alt="profile"
+              />
+            </Link>
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => {
+                    signIn(provider.id)
+                  }}
+                  className="black_btn"
+                >
+                  Sign in
+                </button>
+              ))}
+          </>
+        )}
+      </div>
     </nav>
   )
 }
