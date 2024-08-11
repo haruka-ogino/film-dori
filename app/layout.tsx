e27cc1c223e5d11e78658d3235ddcebd456fedb2
Haruka Ogino <harukariq@gmail.com>
2024-08-11 13:18:23 +1200

    implement provider on layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import './globals.css'
 import NavBar from '@/components/NavBar'
 import { Single_Day } from 'next/font/google'
 import Link from 'next/link'
+import Provider from '@/components/Provider'
+import { SessionProviderProps } from 'next-auth/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,27 +21,31 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  session,
 }: Readonly<{
   children: React.ReactNode
+  session: SessionProviderProps['session']
 }>) {
   return (
     <html lang="en">
       <body
         className={`${inter.className} ${singleDay_init.variable} single_day`}
       >
-        <header className="flex justify-between items-center overflow-x-hidden">
-          <h1 className="text-5xl">All the Ghibli Things!</h1>
-          <Link href="/">
-            {/* img needs alt text despite being decorative as it is providing functionality (link to home page) */}
-            <img
-              className="w-48"
-              src="https://www.ghibli-museum.jp/en/img/mainimg.png"
-              alt="link to home page"
-            />
-          </Link>
-        </header>
-        <NavBar />
-        {children}
+        <Provider session={session}>
+          <header className="flex justify-between items-center overflow-x-hidden">
+            <h1 className="text-5xl">All the Ghibli Things!</h1>
+            <Link href="/">
+              {/* img needs alt text despite being decorative as it is providing functionality (link to home page) */}
+              <img
+                className="w-48"
+                src="https://www.ghibli-museum.jp/en/img/mainimg.png"
+                alt="link to home page"
+              />
+            </Link>
+          </header>
+          <NavBar />
+          {children}
+        </Provider>
       </body>
     </html>
   )
