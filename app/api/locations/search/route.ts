044323bf2305c9e58ffe0279b4c091555366805e
Haruka Ogino <harukariq@gmail.com>
2024-08-12 21:41:53 +1200

    debug search BE route

--- app/api/locations/search/route.ts
+++ app/api/locations/search/route.ts
@@ -1,7 +1,4 @@
-export const GET = async (
-  req: Request,
-  { params }: { params: { search: string } }
-) => {
+export const POST = async (req: Request) => {
   try {
     const apiKey = process.env.GOOGLE_KEY
 
@@ -9,25 +6,31 @@ export const GET = async (
       throw new Error('Google API key is missing')
     }
 
+    const { search } = await req.json()
+
     const res = await fetch(
       `https://places.googleapis.com/v1/places:searchText`,
       {
+        method: 'POST',
         headers: {
           'Content-Type': 'application/json',
           'X-Goog-Api-Key': apiKey,
           'X-Goog-FieldMask':
             'places.id,places.displayName,places.formattedAddress',
         },
-        body: JSON.stringify({ textQuery: params.search }),
+        body: JSON.stringify({ textQuery: search }),
       }
     )
+
     if (!res.ok) {
       throw new Error(`Failed to fetch data: ${res.statusText}`)
     }
 
     const data = await res.json()
-    return new Response(JSON.stringify(data), { status: 200 })
+    return new Response(JSON.stringify(data.places), { status: 200 })
   } catch (error) {
-    return new Response('Failed to fetch search results', { status: 500 })
+    return new Response(`Failed to fetch search results ${error}`, {
+      status: 500,
+    })
   }
 }
