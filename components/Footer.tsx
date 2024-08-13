8a46030642b5d91d0fe3dd3c2f70586dfd035d31
Haruka Ogino <harukariq@gmail.com>
2024-08-13 20:35:59 +1200

    create footer

---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer>
+      <h1>Made by Haruka Ogino</h1>
+      <h4>Website's GitHub Repo:</h4>
+      <a href="https://github.com/haruka-ogino/ghibli">
+        <img
+          src="https://img.shields.io/badge/GitHub-224756?style=fflat-square&logo=github"
+          alt="github banner as a link to this site's repo"
+        />
+      </a>
+    </footer>
+  )
+}
