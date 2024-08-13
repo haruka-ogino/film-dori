5a491f8b793adcb82b588de94da53eee0b197d7c
Haruka Ogino <harukariq@gmail.com>
2024-08-13 17:13:13 +1200

    create future goals page

---
+++ app/goals/page.tsx
@@ -0,0 +1,11 @@
+export default function FutureGoals() {
+  return (
+    <>
+      <h1>Future goals for this app</h1>
+      <h2>
+        Below are some of the features that are part of MVP (minimum viable
+        product), but have not been implemented.
+      </h2>
+    </>
+  )
+}
