bb33c617d10fa8f5db8eb9f593372f1e57443f68
Haruka Ogino <harukariq@gmail.com>
2024-08-16 22:13:23 +1200

    create BE route for tags

---
+++ app/api/tags/route.ts
@@ -0,0 +1,12 @@
+export const revalidate = 0
+
+import { turso } from '@/utils/database'
+
+export const GET = async (req: Request) => {
+  try {
+    const all_tags = await turso.execute(`SELECT * FROM tags`)
+    return new Response(JSON.stringify(all_tags.rows), { status: 200 })
+  } catch (error) {
+    return new Response(`Failed to fetch locations`, { status: 500 })
+  }
+}
