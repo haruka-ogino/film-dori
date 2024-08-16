d43a026879fd59c105578b817f4a0fa69d672bd6
Haruka Ogino <harukariq@gmail.com>
2024-08-16 23:10:38 +1200

    send tag id to BE

--- components/SaveLocation.tsx
+++ components/SaveLocation.tsx
@@ -2,7 +2,7 @@ import { useSaveLocation } from '@/hooks/useLocations'
 import { useTags } from '@/hooks/useTags'
 import { GoogleSearchRes } from '@/models/google-locations'
 import { Session } from 'next-auth'
-import { ChangeEvent, Dispatch, SetStateAction, useState } from 'react'
+import { Dispatch, SetStateAction, useState } from 'react'
 
 interface Props {
   location: GoogleSearchRes | undefined
@@ -16,7 +16,7 @@ export default function SaveLocation({ location, open, session }: Props) {
     image: '',
     description: '',
     authId: '',
-    tag: '',
+    tag_id: 0,
   })
 
   console.log(location)
@@ -24,14 +24,28 @@ export default function SaveLocation({ location, open, session }: Props) {
   const saveLocation = useSaveLocation()
   const { data: tags, isLoading, isError } = useTags()
 
-  function handleChange(e: ChangeEvent<HTMLInputElement>) {
-    const { name, value } = e.target
+  // function handleChange(
+  //   e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  // ) {
+  //   const { name, value, type } = e.target
 
-    setNewLocation((prev) => ({
-      ...prev,
-      [name]: value,
-    }))
-  }
+  //   console.log('Element Type:', type)
+  //   console.log('Value Property:', value)
+
+  //   if (type === 'radio') {
+  //     const input = e.target as HTMLInputElement
+  //     console.log('Checked Property:', input.checked)
+  //     setNewLocation((prev) => ({
+  //       ...prev,
+  //       [name]: input.checked,
+  //     }))
+  //   } else {
+  //     setNewLocation((prev) => ({
+  //       ...prev,
+  //       [name]: value,
+  //     }))
+  //   }
+  // }
 
   function saveNewLocation(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
@@ -39,6 +53,8 @@ export default function SaveLocation({ location, open, session }: Props) {
     if (location && session && session.user) {
       const authId = session.user.id || ''
       setNewLocation((prev) => {
+        console.log(prev)
+
         return { ...prev, id: location.id, authId }
       })
     }
@@ -71,12 +87,14 @@ export default function SaveLocation({ location, open, session }: Props) {
                 <br />
                 <textarea
                   name="description"
-                  onChange={(e) =>
+                  onChange={(e) => {
+                    console.log(e)
+
                     setNewLocation({
                       ...newLocation,
                       description: e.target.value,
                     })
-                  }
+                  }}
                   placeholder="describe location"
                   className="m-3 ml-5 pl-2 h-36 min-w-[20em] rounded-md"
                   required
@@ -91,20 +109,19 @@ export default function SaveLocation({ location, open, session }: Props) {
                     <input
                       type="radio"
                       id={`input-${tag.id}`}
-                      name="tag"
-                      value={tag.tag}
-                      checked={newLocation.tag === tag.tag}
+                      name="tag_id"
+                      value={tag.id}
+                      checked={newLocation.tag_id === tag.id}
                       onChange={(e) => {
-                        console.log(e)
-
+                        const selectedTagId = parseInt(e.target.value, 10)
                         setNewLocation((prev) => ({
                           ...prev,
-                          tag: e.target.value,
+                          tag_id: selectedTagId,
                         }))
                       }}
                       className="mr-2"
                     />
-                    <label htmlFor={tag.tag}>{tag.tag}</label>
+                    <label htmlFor={`input-${tag.id}`}>{tag.tag}</label>
                   </div>
                 ))}
               </div>
@@ -117,9 +134,11 @@ export default function SaveLocation({ location, open, session }: Props) {
             <input
               name="image-url"
               type="text"
-              onChange={(e) =>
+              onChange={(e) => {
+                console.log(e)
+
                 setNewLocation({ ...newLocation, image: e.target.value })
-              }
+              }}
               placeholder="image url"
               className="m-3 ml-5 pl-2 min-w-[32em] rounded-md"
               required
