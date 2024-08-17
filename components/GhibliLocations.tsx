d11ccd53c8430ca3bd054437a9c9f39c5749e97c
Haruka Ogino <harukariq@gmail.com>
2024-08-17 12:22:16 +1200

    style tag on locations page

--- components/GhibliLocations.tsx
+++ components/GhibliLocations.tsx
@@ -16,9 +16,15 @@ const GhibliLocations = () => {
         <h1>Some Ghibli Locations</h1>
         {locations.map((location: Location, i: number) => (
           <section key={i} className="mt-10 mp-10 flex flex-col w-full">
-            <h2 className="text-4xl">{location.name}</h2>
-            <p>{location.tag}</p>
-            <img src={location.image} className="self-center m-3" />
+            <div className="flex flex-wrap justify-left align-center">
+              <h2 className="text-4xl">{location.name}</h2>
+              <p className="tag">{location.tag}</p>
+            </div>
+            <img
+              src={location.image}
+              className="self-center m-3"
+              alt={`Location of ${location.name}`}
+            />
             <p>{location.description}</p>
             <p>Google rating: {location.rating} ⭐️</p>
             <p>Address:</p>
