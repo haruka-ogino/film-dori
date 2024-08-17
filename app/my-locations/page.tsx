edacfeddbdf77ca31c18c757f64d746cc0c1d0be
Haruka Ogino <harukariq@gmail.com>
2024-08-17 13:49:22 +1200

    add auth to my-location page

--- app/my-locations/page.tsx
+++ app/my-locations/page.tsx
@@ -1,10 +1,20 @@
+'use client'
+import { useAuth } from '@/hooks/useProviders'
+
 export default function MyLocations() {
+  const { session } = useAuth()
+
   return (
     <>
-      <h1>My Locations!</h1>
-      <p>~ This page is under construction ~</p>
-      <p>~ Stay tuned for updates ~</p>
-      {/* <p>Login to see your saved and favorutite locations.</p> */}
+      {session?.user ? (
+        <>
+          <h1>My Locations!</h1>
+          <p>~ This page is under construction ~</p>
+          <p>~ Stay tuned for updates ~</p>
+        </>
+      ) : (
+        <p>Login to start saving and sharing locations!.</p>
+      )}
     </>
   )
 }
