073b05731be0c8b4869d364dca3decb7408f03c1
Haruka Ogino <harukariq@gmail.com>
2024-08-22 22:33:22 +1200

    start small-screen navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -63,6 +63,85 @@ const NavBar = () => {
           </>
         )}
       </nav>
+
+      {/* Mobile Navigation */}
+      <nav className="sm:hidden fixed right-0 top-0 flex justify-around transform-translate-x-1/2 py-1 z-10 overflow-hidden">
+        {/* <nav className="sm:hidden fixed right-0 mr-16 top-0 flex justify-around transform -translate-x-1/2 py-1 bg-[#f4fbfe] w-[70%] z-10 box-border overflow-hidden"> */}
+        {!toggleDropdown ? (
+          <div
+            className="relative w-16 pt-1"
+            id=""
+            onClick={() => setToggleDropdown(true)}
+          >
+            <div className="open-icon leading-5 py-2 rounded">
+              <h5>━</h5>
+              <h5>━</h5>
+              <h5>━</h5>
+            </div>
+          </div>
+        ) : (
+          <>
+            <div
+              className="relative small-navbar w-16 pt-1 py-2 rounded"
+              id=""
+              onClick={() => setToggleDropdown(false)}
+            >
+              <h5 className="fixed p-0 top-1.5 leading-3 right-2.5 text-3xl">
+                x
+              </h5>
+            </div>
+            <Link href="/" className="mx-2">
+              Discover
+            </Link>
+            <Link href="/post-location" className="mx-2">
+              Add Location
+            </Link>
+            {session?.user && (
+              <Link href="/my-locations" className="mx-2">
+                My Locations
+              </Link>
+            )}
+            <Link href="/about" className="mx-2">
+              About
+            </Link>
+            {session?.user ? (
+              <div className="flex gap-3 md:gap-5 mx-2">
+                <button
+                  type="button"
+                  className="log-btn"
+                  onClick={() => signOut()}
+                >
+                  Sign Out
+                </button>
+
+                <Image
+                  src={session?.user.image ?? ''}
+                  width={37}
+                  height={37}
+                  className="rounded-full"
+                  alt="profile"
+                />
+              </div>
+            ) : (
+              <>
+                {providers &&
+                  Object.values(providers).map((provider) => (
+                    <button
+                      type="button"
+                      className="log-btn"
+                      key={provider.name}
+                      onClick={() => {
+                        signIn(provider.id)
+                      }}
+                    >
+                      Sign in
+                    </button>
+                  ))}
+              </>
+            )}
+          </>
+        )}
+      </nav>
     </>
   )
 }
