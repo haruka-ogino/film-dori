fae09524b3a09839b16330bb06254cb22bdc78b8
Haruka Ogino <harukariq@gmail.com>
2024-08-25 20:42:05 +1200

    update query key

--- hooks/useLocations.ts
+++ hooks/useLocations.ts
@@ -20,7 +20,7 @@ export function useSaveLocation() {
 export function useLocations(authId: string, tagId: number) {
   const query = useQuery({
     queryFn: async () => getLocations(authId, tagId),
-    queryKey: ['my-locations', authId, tagId, 'locations'],
+    queryKey: ['locations', 'my-locations'],
   })
   return {
     ...query,
