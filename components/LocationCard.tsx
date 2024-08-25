de113f72f0bc98d855bc6907e0d0ce6a9ba84aad
Haruka Ogino <harukariq@gmail.com>
2024-08-25 17:41:21 +1200

    implement popUp logic

--- components/LocationCard.tsx
+++ components/LocationCard.tsx
@@ -1,6 +1,8 @@
 import { useDeleteLocation } from '@/hooks/useLocations'
 import { Location } from '@/models/locations'
+import { useState } from 'react'
 import { FaRegTrashAlt, FaEdit } from 'react-icons/fa'
+import EditPopUp from './EditPopUp'
 
 interface Params {
   location: Location
@@ -20,10 +22,29 @@ export default function LocationCard({
   i,
 }: Params) {
   const deleteLocation = useDeleteLocation()
+  const [popUp, setPopUp] = useState(false)
 
   function handleDelete(id: string) {
     deleteLocation.mutate({ id, authId })
   }
+
+  // const example = {
+  //   address: 'Japan, 〒206-0013 Tokyo, Tama, Sakuragaoka, 4-chōme−４３−２５',
+  //   authId: '110783889429574267521',
+  //   description:
+  //     'A neighborhood that inspired locations in "Whisper of the Heart"',
+  //   id: 'ChIJmbzCFl_jGGAR98V28VrP0QI',
+  //   image:
+  //     'https://d20aeo683mqd6t.cloudfront.net/articles/title_images/000/040/281/original/cover-photo.jpg',
+  //   name: 'Iroha-zaka Slope',
+  //   rating: 4.3,
+  //   tag: 'ghibli',
+  //   tagId: 1,
+  //   url: 'https://maps.google.com/?cid=203171447713482231',
+  //   userImg:
+  //     'https://lh3.googleusercontent.com/a/ACg8ocKUGaeHdEfoH0Gnzt7zQEwLL_mFhsbttpEXptq7TmqGPSl_Ev8=s96-c',
+  //   username: 'davidw',
+  // }
 
   return (
     <section key={i} className="mt-10 flex flex-col w-full">
@@ -37,19 +58,22 @@ export default function LocationCard({
         </p>
       </div>
       <div className="relative flex justify-center m-3">
-        {title === 'My Locations' && (
-          <div className="absolute p-[5px_12px] m-[5px] self-end flex top-[-57px] right-0 tag">
-            <button className="cursor-pointer hover:opacity-50 pr-4">
-              <FaEdit size={25} />
-            </button>
-            <button
-              className="cursor-pointer hover:opacity-50"
-              onClick={() => handleDelete(location.id)}
-            >
-              <FaRegTrashAlt size={25} />
-            </button>
-          </div>
-        )}
+        {/* {title === 'My Locations' && ( */}
+        <div className="absolute p-[5px_12px] m-[5px] self-end flex top-[-57px] right-0 tag">
+          <button
+            className="cursor-pointer hover:opacity-50 pr-4"
+            onClick={() => setPopUp(true)}
+          >
+            <FaEdit size={25} />
+          </button>
+          <button
+            className="cursor-pointer hover:opacity-50"
+            onClick={() => handleDelete(location.id)}
+          >
+            <FaRegTrashAlt size={25} />
+          </button>
+        </div>
+        {/* )} */}
         <img src={location.image} alt={`Location of ${location.name}`} />
       </div>
       <p>{location.description}</p>
@@ -68,6 +92,7 @@ export default function LocationCard({
       ) : (
         <p className="self-center">By {location.username}</p>
       )}
+      {popUp && <EditPopUp location={location} open={setPopUp} />}
     </section>
   )
 }
