2db5b37d7957e62dd8ce27df9b18c994eb3a723d
Haruka Ogino <harukariq@gmail.com>
2024-08-30 14:42:58 +1200

    finalise BE route to get AI response

--- app/api/locations/search/description/route.ts
+++ app/api/locations/search/description/route.ts
@@ -1,7 +1,10 @@
 import { GoogleGenerativeAI } from '@google/generative-ai'
 
-export const GET = async (req: Request) => {
+export const POST = async (req: Request) => {
   try {
+    // receive data
+    const { locationInfo: location_info } = await req.json()
+
     // api checks
     const googleKey = process.env.GOOGLE_AI_KEY
 
@@ -12,10 +15,14 @@ export const GET = async (req: Request) => {
     const genAI = new GoogleGenerativeAI(googleKey)
     const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' })
 
-    const prompt = 'Write a story about a magic backpack.'
+    // use line below to add some configurations - see docs
+    // const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash', varName })
+
+    const prompt = `Write a brief (circa three sentences) about the location: ${location_info}`
 
     const result = await model.generateContent(prompt)
-    console.log(result.response.text())
+
+    return new Response(JSON.stringify(result.response.text()), { status: 200 })
   } catch (error) {
     return new Response(`Failed to fetch response ${error}`, {
       status: 500,
