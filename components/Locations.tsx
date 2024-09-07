8d77afc0893524fddd1cc4eceb49fa1fa0d7f257
Haruka Ogino <harukariq@gmail.com>
2024-09-07 16:28:15 +1200

    refactor conditionals for displaying tags

--- components/Locations.tsx
+++ components/Locations.tsx
@@ -3,7 +3,6 @@ import { useQueryClient } from '@tanstack/react-query'
 import { Dispatch, SetStateAction } from 'react'
 import MediaTag from './MediaTag'
 import UserTag from './UserTag'
-import Link from 'next/link'
 import LocationCard from './LocationCard'
 
 interface Params {
@@ -51,28 +50,13 @@ export default function Locations({
     <>
       <h1 className="text-5xl">{title}</h1>
 
-      {title === 'My Locations' && locations.length === 0 && (
-        <Link href="post-location">
-          <p>Start sharing locations!</p>
-        </Link>
-      )}
-      {title === 'My Locations' && tag !== 0 && (
-        <MediaTag tag={locations[0].tag} setTag={setTag} />
-      )}
-      {setAuthId &&
-        (tag !== 0 && authId !== 'x' ? (
-          <div className="flex justify-center items-end flex-wrap">
-            <MediaTag tag={locations[0].tag} setTag={setTag} />
-            <UserTag username={locations[0].username} setId={setAuthId} />
-          </div>
-        ) : tag === 0 && authId !== 'x' ? (
-          <div className="flex justify-center items-end flex-wrap">
-            <UserTag username={locations[0].username} setId={setAuthId} />
-          </div>
-        ) : (
-          tag !== 0 &&
-          authId === 'x' && <MediaTag tag={locations[0].tag} setTag={setTag} />
-        ))}
+      <div className="flex justify-center items-end flex-wrap">
+        {tag !== 0 && <MediaTag tag={locations[0].tag} setTag={setTag} />}
+        {setAuthId && authId !== 'x' && (
+          <UserTag username={locations[0].username} setId={setAuthId} />
+        )}
+      </div>
+
       {locations.map((location: Location, i: number) => (
         <section key={i} className="mt-10 flex flex-col w-full">
           <LocationCard
