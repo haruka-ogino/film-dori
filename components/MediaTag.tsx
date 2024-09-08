b3e1ab7bed1655b30db0fa687cdc9d09e3579948
Haruka Ogino <harukariq@gmail.com>
2024-09-08 14:55:57 +1200

    remove commented code

--- components/MediaTag.tsx
+++ components/MediaTag.tsx
@@ -6,7 +6,6 @@ interface Props {
 }
 export default function MediaTag({ tag, setTag }: Props) {
   return (
-    // <p className="gradient rounded-[20px] w-fit leading-[25px] text-[25px] m-0 p-[5px_10px]">
     <p className="gradient rounded-full w-fit m-0 px-2">
       {tag}
       <button onClick={() => setTag(0)}>
