879aacf9343ba2fc6c5ad230c031b1eab383cdc8
Haruka Ogino <harukariq@gmail.com>
2024-09-10 12:51:28 +1200

    set up Location test comp

---
+++ test/Locations.test.tsx
@@ -0,0 +1,42 @@
+import { render, screen } from '@testing-library/react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import Locations from '@/components/Locations'
+import '@testing-library/jest-dom'
+import { Location } from '@/models/locations'
+
+const mockLocation = {
+  address: 'Japan, 〒206-0013 Tokyo, Tama, Sakuragaoka, 4-chōme−４３−２５',
+  authId: '123',
+  description:
+    'A neighborhood that inspired locations in "Whisper of the Heart"',
+  id: 'ChIJmbzCFl_jGGAR98V28VrP0QI',
+  image:
+    'https://d20aeo683mqd6t.cloudfront.net/articles/title_images/000/040/281/original/cover-photo.jpg',
+  name: 'Iroha-zaka Slope',
+  rating: 4.3,
+  tag: 'ghibli',
+  tagId: 1,
+  url: 'https://maps.google.com/?cid=203171447713482231',
+  userImg: 'image-link',
+  username: 'koda',
+}
+
+const mockSetTag = jest.fn()
+const tag = 0
+const mockAuthId = '123'
+
+function renderCard(mockTitle: string, mockLocations: Location[]) {
+  const queryClient = new QueryClient()
+
+  render(
+    <QueryClientProvider client={queryClient}>
+      <Locations
+        locations={mockLocations}
+        title={mockTitle}
+        setTag={mockSetTag}
+        tag={tag}
+        authId={mockAuthId}
+      />
+    </QueryClientProvider>
+  )
+}
