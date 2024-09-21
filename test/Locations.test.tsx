0647c909d637ed5b3a9bd8230bd065b4254a013d
Haruka Ogino <harukariq@gmail.com>
2024-09-21 21:51:32 +1200

    remove api mock

--- test/Locations.test.tsx
+++ test/Locations.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import userEvent from '@testing-library/user-event'
 import Locations from '@/components/Locations'
 import '@testing-library/jest-dom'
 import { Location } from '@/models/locations'
