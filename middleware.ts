baa8b28adda5352717bb1ed629ba4a68f573b826
Haruka Ogino <harukariq@gmail.com>
2024-09-21 14:08:44 +1200

    debug middleware

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,3 @@
 export { default } from 'next-auth/middleware'
 
-export const config = { matcher: ['/api/locations/:authId/by-id/:id'] }
+export const config = { matcher: ['/api/locations/:authId/by-id/:id*'] }
